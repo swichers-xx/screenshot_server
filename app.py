ee6cd767e49e31f89643d5548a745fad92c38487
swichers-xx <seanwichers@gmail.com>
2023-11-22 04:27:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from datetime import datetime
 import threading
@@ -10,32 +9,23 @@ import base64
 
 app = Flask(__name__)
 
-# Update the ChromeDriver path according to your setup
-CHROMEDRIVER_PATH = 'C:\Tools\cap2\server\chromedriver.exe'
-
 def url_to_filename(url, extension):
     sanitized_url = "".join([c if c.isalnum() else "_" for c in url])
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     return f"{sanitized_url}_{timestamp}{extension}"
 
 def process_webpage(url):
+    driver = None
     try:
         options = Options()
         options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-    driver = webdriver.Remote(
-    command_executor='http://selenium-hub:4444/wd/hub',
-    options=options
-)
+        # Using environment variable for Selenium Hub URL
+        selenium_hub_url = os.environ.get('SELENIUM_HUB_URL', 'http://172.16.1.184:4444/wd/hub')
+        driver = webdriver.Remote(command_executor=selenium_hub_url, options=options)
 
-
-#        service = Service("C:\Tools\cap2\server\chromedriver.exe")
-#        driver = webdriver.Chrome(service=service, options=options)
-        
-         
-      
         driver.get(url)
         driver.implicitly_wait(10)  # Wait for the page to load
 
@@ -59,6 +49,7 @@ def process_webpage(url):
         text_filename = f'screenshots/{url_to_filename(url, ".txt")}'
         with open(text_filename, 'w', encoding='utf-8') as f:
             f.write(text_content)
+
 
     except Exception as e:
         print(f"Error processing webpage {url}: {e}")
